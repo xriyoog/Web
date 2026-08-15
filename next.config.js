f4fa94bf527a075809d70f2615379263b88d9c81
xriyoog <xriyoog@duck.com>
2026-08-15 21:58:40 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // Crucial for Docker/Railway deployment
+  output: 'standalone',
   experimental: {
     serverActions: true,
   },
